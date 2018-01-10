e48b48c888c97d4e5e0a5049f06a41d56677f391
Young, Mark <mark.d.young@cdk.com>
2018-01-10 08:18:32 +0000

    Add project files.

---
+++ t2tWinFormAppBarLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t2tWinFormAppBarLib
+{
+    public class Class1
+    {
+    }
+}
