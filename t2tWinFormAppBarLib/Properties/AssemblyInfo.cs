bc1dfcbe2c3054a7a3f9c855c7e66c9b0f4f67e3
Young, Mark <mark.d.young@cdk.com>
2018-01-16 21:36:50 +0000

    1.0.2

--- t2tWinFormAppBarLib/Properties/AssemblyInfo.cs
+++ t2tWinFormAppBarLib/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("t2tWinFormAppBarLib")]
 [assembly: AssemblyDescription("AppBar helper library for WinForms")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("tip2tail Ltd")]
+[assembly: AssemblyCompany("Mark Young (tip2tail)")]
 [assembly: AssemblyProduct("t2tWinFormAppBarLib")]
 [assembly: AssemblyCopyright("Copyright © 2018 tip2tail Ltd")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
